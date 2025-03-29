9ab2d128dd991a35e9de399bbeb2cf347ea8134a
hattiza <quiet.iron8712@fastmail.com>
2025-03-29 14:19:31 -0700

    Add 2nd html route/page

--- follow_along/001_004/program/routes.py
+++ follow_along/001_004/program/routes.py
@@ -6,3 +6,8 @@ from flask import render_template
 @app.route("/index")
 def index():
     return render_template("index.html", title="Home Page")
+
+
+@app.route("/100days")
+def hundred_days():
+    return render_template("100Days.html", title="100 Days of Code")
